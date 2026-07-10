dd32d67f64756efa6101d17da588a9d6af2247c9
MuhiddinW89 <altf498@mail.ru>
2026-07-10 15:50:25 +0500

    sales service implementation

--- internal/sales/errors.go
+++ internal/sales/errors.go
@@ -3,5 +3,8 @@ package sales
 import "errors"
 
 var (
-	ErrSaleNotFound = errors.New("sale not found")
+	ErrSaleNotFound        = errors.New("sale not found")
+	ErrInvalidSalePrice    = errors.New("price must be greater than zero")
+	ErrInvalidVolume       = errors.New("volume must be greater than zero")
+	ErrPerfumeNameRequired = errors.New("perfume name is required")
 )
--- internal/sales/service_impl.go
+++ internal/sales/service_impl.go
@@ -2,19 +2,14 @@ package sales
 
 import (
 	"context"
+	"time"
+
 	"github.com/google/uuid"
 )
 
 type service struct {
 	repository Repository
 }
-
-// Create implements [Service].
-func (s *service) Create(ctx context.Context, input CreateSaleInput) (*Sale, error) {
-	panic("unimplemented")
-}
-
-
 
 func NewService(
 	repository Repository,
@@ -23,3 +18,47 @@ func NewService(
 		repository: repository,
 	}
 }
+
+// GetByID implements [Service].
+func (s *service) GetByID(ctx context.Context, id uuid.UUID) (*Sale, error) {
+	panic("unimplemented")
+}
+
+// ListByClientID implements [Service].
+func (s *service) ListByClientID(ctx context.Context, clientID uuid.UUID) ([]*Sale, error) {
+	panic("unimplemented")
+}
+
+// Create implements [Service].
+func (s *service) Create(ctx context.Context, input CreateSaleInput) (*Sale, error) {
+	if input.Price <= 0 {
+		return nil, ErrInvalidSalePrice
+	}
+
+	if input.VolumeML <= 0 {
+		return nil, ErrInvalidVolume
+	}
+
+	if input.PerfumeName == "" {
+		return nil, ErrPerfumeNameRequired
+	}
+
+	sale := &Sale{
+		ID:	uuid.New(),
+		ClientID:	input.ClientID,
+		PerfumeName: input.PerfumeName,
+		VolumeML:	input.VolumeML,
+		Price:	input.Price,
+		Comment:	input.Comment,
+		SaleDate:	input.SaleDate,
+		CreatedAt:	time.Now(),
+		UpdatedAt:	time.Now(),
+	}
+
+	if err := s.repository.Create(ctx, sale); err !=nil {
+		return nil, err
+	}
+
+	return sale, nil
+}
+
