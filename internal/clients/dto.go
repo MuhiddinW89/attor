eeccee284d7644e63ee2095749799e171130b7ad
MuhiddinW89 <altf498@mail.ru>
2026-08-04 17:38:13 +0500

    json full_name fixed

--- internal/clients/dto.go
+++ internal/clients/dto.go
@@ -1,29 +1,28 @@
 package clients
 
 type CreateClientRequest struct {
-	FullName  string  `json:"fullName"`
+	FullName  string  `json:"full_name"`
 	Phone     string  `json:"phone"`
 	Instagram *string `json:"instagram"`
 }
 
 type ClientResponse struct {
 	ID        string  `json:"id"`
-	FullName  string  `json:"fullName"`
+	FullName  string  `json:"full_name"`
 	Phone     string  `json:"phone"`
 	Instagram *string `json:"instagram,omitempty"`
 }
 
 type ClientListItem struct {
 	ID       string `json:"id"`
-	FullName string `json:"fullName"`
+	FullName string `json:"full_name"`
 	Phone    string `json:"phone"`
 }
 
 type ClientDetailsResponse struct {
-	ID         string  `json:"id"`
-	FullName   string  `json:"fullName"`
-	Phone      string  `json:"phone"`
-	Instagram  *string `json:"instagram,omitempty"`
-	BirthDate  *string `json:"birthDate,omitempty"`
+	ID        string  `json:"id"`
+	FullName  string  `json:"full_name"`
+	Phone     string  `json:"phone"`
+	Instagram *string `json:"instagram,omitempty"`
+	BirthDate *string `json:"birthDate,omitempty"`
 }
-
--- internal/clients/model.go
+++ internal/clients/model.go
@@ -7,11 +7,11 @@ import (
 )
 
 type Client struct {
-	ID         uuid.UUID  `json:"id"`
-	FullName   string     `json:"fullName"`
-	Phone      string     `json:"phone"`
-	Instagram  *string    `json:"instagram,omitempty"`
-	BirthDate  *time.Time `json:"birthDate,omitempty"`
-	CreatedAt  time.Time  `json:"createdAt"`
-	UpdatedAt  time.Time  `json:"updatedAt"`
+	ID        uuid.UUID  `json:"id"`
+	FullName  string     `json:"full_name"`
+	Phone     string     `json:"phone"`
+	Instagram *string    `json:"instagram,omitempty"`
+	BirthDate *time.Time `json:"birthDate,omitempty"`
+	CreatedAt time.Time  `json:"createdAt"`
+	UpdatedAt time.Time  `json:"updatedAt"`
 }
